c2a415385b23d741aff83cab2a33a8f680850cf5
caodonglei-kavout <caodonglei@gmail.com>
2022-07-11 15:25:20 +0800

    Create error.rs

---
+++ blockchain_v006/error.rs
@@ -0,0 +1,11 @@
+use thiserror::Error;
+
+/* 错误信息 */
+#[derive(Debug, Error)]
+pub enum BlockchainError {
+    #[error("Serialize or Deserialize error")]
+    SerializeError(#[from] Box<bincode::ErrorKind>),
+
+    #[error("Failed to access rocks db")]
+    RocksDbError(#[from] rocksdb::Error),
+}
